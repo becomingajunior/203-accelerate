a83d6cb27244a234ef8b7f43682d7396826bf910
Weber <jweber617@gmail.com>
2019-08-20 14:46:55 -0700

    second deploy

--- wp-content/themes/accel-theme-child/front-page.php
+++ wp-content/themes/accel-theme-child/front-page.php
@@ -2,10 +2,6 @@
 /**
  * The template for displaying the homepage
  *
- * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages
- * and that other 'pages' on your WordPress site will use a
- * different template.
  *
  * @package WordPress
  * @subpackage Accelerate Marketing
@@ -18,22 +14,46 @@ get_header(); ?>
 		<div class="main-content" role="main">
 			<?php while ( have_posts() ) : the_post(); ?>
 				<?php the_content(); ?>
-				<a class="button" href="<?php echo site_url('/blog/') ?>">View Our Work</a>
+				<a class="button" href="<?php echo site_url('/case-studies/') ?>">View Our Work</a>
 			<?php endwhile; // end of the loop. ?>
 		</div><!-- .main-content -->
 	</div><!-- #primary -->
 
-	<section class="recent-posts">
- 		<div class="site-content">
- 			<div class="blog-post">
-				<h4>From the Blog</h4>
-					<?php query_posts('posts_per_page=1'); ?>
-					<?php while ( have_posts() ) : the_post(); ?> 
-						<h3><?php the_title(); ?></h3>
-       					<?php the_excerpt(); ?> 
-  					<?php endwhile; ?> 
-					<?php wp_reset_query(); ?>
-			</div>   
+<section class="featured-work">
+	<div class="site-content">
+		<h4>Featured Work</h4>
+
+		<ul class="homepage-featured-work">
+		<?php query_posts('posts_per_page=3&post_type=case_studies'); ?>
+				<?php while ( have_posts() ) : the_post(); 
+					$image_1 = get_field("image_1");
+					$size = "medium";
+				?> 
+
+				<li class="individual-featured-work">
+				<figure>
+					<?php echo wp_get_attachment_image($image_1, $size); ?>
+				</figure>	
+					<h5><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
+				</li>
+
+					<?php endwhile; // end of the loop. ?> 
+			<?php wp_reset_query(); ?>
+		</ul>	
+	</div>
+</section>
+
+<section class="recent-posts">
+ 	<div class="site-content">
+ 		<div class="blog-post">
+			<h4>From the Blog</h4>
+				<?php query_posts('posts_per_page=1'); ?>
+				<?php while ( have_posts() ) : the_post(); ?> 
+					<h3><?php the_title(); ?></h3>
+       				<?php the_excerpt(); ?> 
+  				<?php endwhile; // end of the loop. ?> 
+					
+		</div>   
  	</div>
 </section>
 
